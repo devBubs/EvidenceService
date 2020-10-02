080322282826d5c7f490b8258b28cd4ddf27e460
shradhashankar <shradha.shankar26@gmail.com>
2020-10-02 15:20:12 +0530

    Initial Commit

---
+++ src/main/java/c2/elastic/bucket/EvidenceService/EvidenceServiceApplication.java
@@ -0,0 +1,13 @@
+package c2.elastic.bucket.EvidenceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidenceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c2/elastic/bucket/EvidenceService/EvidenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package c2.elastic.bucket.EvidenceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvidenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
